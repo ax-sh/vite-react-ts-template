3741eb714fe1933e475ed9898a958f1ce17584f7
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-12-03 09:41:20 +0545

    feat: add tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,13 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: [],
+  content: [
+      './src/modules/**/*.{js,ts,jsx,tsx}',
+      './src/pages/**/*.{js,ts,jsx,tsx}',
+      './src/**/*.{js,ts,jsx,tsx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
