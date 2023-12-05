8efd2a8a00649821ce2b430a02fb5b47c3fffd5c
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-12-06 04:15:48 +0545

    feat: add pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import Pages from 'vite-plugin-pages'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), Pages()],
+	plugins: [VitePWA(), react(), Pages()],
 	server: {
 		host: true
 	}
