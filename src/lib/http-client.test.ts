ca7f84052fccac5fc9bb38448d6806324a74f2ad
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-12-05 14:09:57 +0545

    feat: add api tests

--- src/lib/http-client.test.ts
+++ src/lib/http-client.test.ts
@@ -1,12 +1,10 @@
 import { httpClient } from './http-client.ts'
 import { setupServer } from 'msw/node'
 import { http, HttpResponse } from 'msw'
+import { AxiosError, isAxiosError } from 'axios'
+import { expect } from 'vitest'
 
-const server = setupServer(
-	http.get('/test', () => {
-		return HttpResponse.json({ message: 'success' })
-	})
-)
+const server = setupServer(http.get('/test', () => HttpResponse.json({ message: 'success' })))
 
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
@@ -28,5 +26,20 @@ describe(httpClient.name, () => {
 		const response = await httpClient.get('/test')
 		const data = await response.data
 		expect(data).toEqual({ message: 'success' })
+	})
+
+	it('should fail api response', async () => {
+		server.use(http.get('/test', () => HttpResponse.json({ message: 'fail' }, { status: 500 })))
+		try {
+			const response = await httpClient.get('/test')
+			console.log(response)
+		} catch (e) {
+			if (!isAxiosError(e)) return
+			const err: AxiosError = e
+			const response = err.response
+			const data = response?.data
+			console.log(data)
+			expect(data).toEqual({ message: 'fail' })
+		}
 	})
 })
