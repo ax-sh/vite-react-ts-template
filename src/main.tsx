378373f94451e34853e74f119e3eae55c467c768
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-12-05 13:44:38 +0545

    feat: add msw browser.ts

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,29 @@ import './global.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+async function enableMocking() {
+	if (process.env.NODE_ENV !== 'development') {
+		return
+	}
+
+	const { worker } = await import('./mocks/browser')
+
+	// `worker.start()` returns a Promise that resolves
+	// once the Service Worker is up and ready to intercept requests.
+	return worker.start()
+}
 
 const queryClient = new QueryClient()
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<Router>
-				<App />
-			</Router>
-			<ReactQueryDevtools initialIsOpen={false} />
-		</QueryClientProvider>
-	</React.StrictMode>
-)
+
+enableMocking().then(() => {
+	ReactDOM.createRoot(document.getElementById('root')!).render(
+		<React.StrictMode>
+			<QueryClientProvider client={queryClient}>
+				<Router>
+					<App />
+				</Router>
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
+		</React.StrictMode>
+	)
+})
