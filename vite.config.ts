0e036a8daa6d5f38569b61637f5c00942f67cd0b
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-12-10 15:31:51 +0545

    feat: add vite feature/vite-banner

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import banner from 'vite-plugin-banner'
 import Pages from 'vite-plugin-pages'
 import { VitePWA } from 'vite-plugin-pwa'
 
@@ -6,7 +7,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [VitePWA(), react(), Pages()],
+	plugins: [banner('AX-SH TEMPLATE'), VitePWA(), react(), Pages()],
 	server: {
 		host: true
 	}
