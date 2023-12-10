d78c5b92585a20d77d004023f3c376dd1c41565b
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-12-10 15:32:59 +0545

    feat: add vite analyitics

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,24 @@ import { defineConfig } from 'vite'
 import banner from 'vite-plugin-banner'
 import Pages from 'vite-plugin-pages'
 import { VitePWA } from 'vite-plugin-pwa'
+import VitePluginRadar from 'vite-plugin-radar'
 
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [banner('AX-SH TEMPLATE'), VitePWA(), react(), Pages()],
+	plugins: [
+		VitePluginRadar({
+			// Google Analytics tag injection
+			analytics: {
+				id: 'G-XXXXX'
+			}
+		}),
+		banner('AX-SH TEMPLATE'),
+		VitePWA(),
+		react(),
+		Pages()
+	],
 	server: {
 		host: true
 	}
