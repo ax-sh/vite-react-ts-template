6f18504d0114d01d889cb90f5a961772513daf89
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-12-17 17:33:34 +0545

    docs: add vite checker

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ async function enableMocking() {
 	if (process.env.NODE_ENV !== 'development') return
 
 	const { worker } = await import('./mocks/browser')
-
+	// NEXT_PUBLIC_MOCK_API
 	// `worker.start()` returns a Promise that resolves
 	// once the Service Worker is up and ready to intercept requests.
 	return worker.start()
